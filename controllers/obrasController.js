bf38764d5c9662262225b68f72af5fe15ff9b8b6
Julian Zamt <julizamt@gmail.com>
2021-05-23 13:00:06 -0300

    CRUD operations

--- controllers/obrasController.js
+++ controllers/obrasController.js
@@ -1,39 +1,61 @@
-const obraModel = require('../models/obraModel')
+const obraModel = require("../models/obraModel");
 
 module.exports = {
-    getAll: async function (req, res, next) {
-        try {
-            const obras = await obraModel.find()
-            res.json(obras)
-        } catch (e) {
-            e.status = 400
-            console.log("culo")
-            next(e) // Generic error Handler in app.js
-        }
-    },
-    create: async function (req, res, next) {
-        try {
-            const document = new obraModel({
-                name: req.body.name,
-                year: req.body.date,
-                mainImage: req.body.mainImage,
-                text: req.body.text,
-                hidden: req.body.hidden
-            })
-            const newObra = await document.save()
-            res.json(newObra)
-        } catch (e) {
-            e.status = 400
-            next(e)
-        }
-    },
-    getById: async function (req, res, next) {
-        try {
-            const obra = await obraModel.findById(req.params.id)
-            res.json(obra)
-        } catch (e) {
-            e.status = 400
-            next(e)
-        }
+  getAll: async function (req, res, next) {
+    try {
+      console.log("Hola");
+      const obras = await obraModel.find();
+      res.json(obras);
+    } catch (e) {
+      e.status = 400;
+      next(e); // Generic error Handler in app.js
     }
-}
+  },
+  create: async function (req, res, next) {
+    try {
+      const document = new obraModel({
+        name: req.body.name,
+        year: req.body.date,
+        mainImage: req.body.mainImage,
+        text: req.body.text,
+        hidden: req.body.hidden,
+      });
+      const newObra = await document.save();
+      res.json(newObra);
+    } catch (e) {
+      e.status = 400;
+      next(e);
+    }
+  },
+  getById: async function (req, res, next) {
+    try {
+      console.log(req.params);
+      const obra = await obraModel.findById(req.params.id);
+      res.json(obra);
+    } catch (e) {
+      e.status = 400;
+      next(e);
+    }
+  },
+  deleteById: async function (req, res, next) {
+    try {
+      const response = await obraModel.deleteOne({ _id: req.params.id });
+      res.json(response);
+    } catch (e) {
+      e.status = 400;
+      next(e);
+    }
+  },
+  update: async function (req, res, next) {
+    try {
+      const response = await obraModel.updateOne(
+        { _id: req.params.id },
+        req.body
+      );
+      res.json(response);
+    } catch (e) {
+      e.status = 400;
+      next(e);
+    }
+  },
+};
--- routes/obras.js
+++ routes/obras.js
@@ -1,9 +1,17 @@
 var express = require("express");
 var router = express.Router();
-const { getAll, create, getById } = require("../controllers/obrasController");
+const {
+  getAll,
+  create,
+  getById,
+  deleteById,
+  update,
+} = require("../controllers/obrasController");
 
 router.post("/", create);
 router.get("/", getAll);
 router.get("/:id", getById);
+router.delete("/:id", deleteById);
+router.post("/:id", update);
 
 module.exports = router;
