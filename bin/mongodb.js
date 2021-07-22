3f8b6e610394f8cfed0328c1f75fdbfd351f7d2f
Julian Zamt <julizamt@gmail.com>
2021-07-22 17:01:20 -0300

    Connected to Atlas

--- bin/mongodb.js
+++ bin/mongodb.js
@@ -1,11 +1,25 @@
 var mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/caza", { useNewUrlParser: true, useUnifiedTopology: true }, function (error) {
-  if (error) {
-    throw error;
-  } else {
-    console.log("Conectado a MongoDB");
+// Local
+// mongoose.connect("mongodb://localhost/caza", { useNewUrlParser: true, useUnifiedTopology: true }, function (error) {
+//   if (error) {
+//     throw error;
+//   } else {
+//     console.log("Conectado a MongoDB");
+//   }
+// });
+
+// Mongodb Atlas
+mongoose.connect(
+  "mongodb+srv://julianzamt:Gualicho2@cluster0.iunbi.mongodb.net/caza?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (error) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Conectado a MongoDB");
+    }
   }
-});
+);
 
 module.exports = mongoose;
